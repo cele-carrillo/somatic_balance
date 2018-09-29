3cf476ac87d7c4dbf0d4c40a8ecfa5aabfde95ab
Sebastian Tello <argos83@gmail.com>
2018-09-29 18:38:43 +1000

    fix: remove team section

--- js/custom.js
+++ js/custom.js
@@ -23,18 +23,6 @@
         }
     });
 
-    /* Team slideshow
-    ----------------------------------------------*/
-    $("#team-carousel").owlCarousel({
- 
-        autoPlay: 5000, //Set AutoPlay to 5 seconds
-
-        items : 4,
-        itemsDesktopSmall : [979,3],
-        stopOnHover: true
- 
-    });
-
     /* Testimonials slideshow
     ----------------------------------------------*/
     $("#testimonial-carousel").owlCarousel({
